56cd14cf58348b7da1793c04960350e0e45515cb
Natalia Lankevich <nataliiks@rambler.ru>
2022-02-24 20:21:27 +0300

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,39 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+
+const arr =[];
+for (let i = 0; i< expr.length; i= i+10) {
+    
+    let j = expr.slice(i, i+10);
+    if (j === "**********") {
+        arr.push('  ');
+    } else {
+        arr.push(Number(j).toString().split(''));
+    }
+
+}
+
+function convertNumToSymbol(item) {
+    let symbols =[]; 
+    for (let i=0; i<item.length; i=i+2){
+       if(item[i]+item[i+1] === '10') {
+        symbols.push('.')
+       };
+       if(item[i]+item[i+1] === '11') {
+        symbols.push('-')
+    };
+       if(item[i]+item[i+1] === '**') {
+        symbols.push(' ')
+    };
+    }
+    return symbols;
+}  
+
+const strSymbols = arr.map((item) => convertNumToSymbol(item).join(''));
+return result = strSymbols.map((item) => MORSE_TABLE[item] || ' ').join('');
+
+/*console.log(result);*/
 }
 
 module.exports = {
